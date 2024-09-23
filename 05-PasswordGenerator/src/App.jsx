3addce35b7577b7fd2555f8bc63ae785c91ced68
Babita00 <babitaasth07@gmail.com>
2024-09-23 17:07:55 +0545

    css add

--- 05-PasswordGenerator/src/App.jsx
+++ 05-PasswordGenerator/src/App.jsx
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { useState } from 'react'
-// import './App.css'
+import { useEffect } from 'react';
+import './App.css'
 
 function App() {
 const [length,setLength]=useState(8)
@@ -27,13 +28,48 @@ for (let i = 1; i < length; i++) {
 setPassword(pass); 
 },[length,number,character,setPassword])
 
+
+useEffect(()=>{
+passwordGenerator()
+},[length,number,character,passwordGenerator])
+
+
   return (
     <>
       
       
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-10 text-orange-500 bg-black'>
-      test
+      <div className='Container'>
+        <div className="inputfield">
+        <input type="text" value={password} placeholder='password' readOnly/>
+        <button>Copy</button>
+        </div>
+
+
+        <div className="newContainer">
+
+        <label className="length" htmlFor="lengthRange">Length:{length}</label>
+        <input type="range" min="8"max="50"value={length} onChange={(e)=>{
+          setLength(e.target.value)
+        }}/>
+        
+        
+    <div className="flex items-center mb-2"> {/* Flex for alignment */}
+        <input type="checkbox" defaultChecked={number} onChange={() => setNumber((prev) => !prev)} />
+        <label className="numbers" htmlFor="numberInput">Numbers</label>
     </div>
+
+
+
+    <div className="flex items-center"> {/* Add margin here */}
+        <input type="checkbox" defaultChecked={character} onChange={() => setCharacter((prev) => !prev)} />
+
+        <label className="characters ml-4" htmlFor="characterInput">Characters</label> {/* Tailwind margin-left */}
+    </div>
+      
+        </div>
+
+
+      </div>
     </>
     
   )
