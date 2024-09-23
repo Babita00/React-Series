5382abfe4a64cb4ad71c756650b1816c2390837b
Babita00 <babitaasth07@gmail.com>
2024-09-23 12:24:26 +0545

    update

--- 5.PasswordGenerator/src/App.jsx
+++ 5.PasswordGenerator/src/App.jsx
@@ -6,9 +6,15 @@ function App() {
   return (
     <>
       
-      <h1>Password generator</h1>
+      
+      <div className="App">
+      <h1 >Password generator</h1>
+      <h1 className="text-red-500">Password generator</h1>
+    </div>
     </>
+    
   )
 }
 
 export default App
+
