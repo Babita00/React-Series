adac5350e5b05b13be2ed5a86a05354272713ba4
Babita00 <babitaasth07@gmail.com>
2024-09-27 13:12:29 +0545

    update css

--- 06-CurrencyConverter/src/App.jsx
+++ 06-CurrencyConverter/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-     <h1 className=' bg-orange-500'>Currency Converter App</h1>
+     <h1 className='text-4xl text-blue-500 text-center pt-8'>Currency Converter App</h1>
     </>
   )
 }
