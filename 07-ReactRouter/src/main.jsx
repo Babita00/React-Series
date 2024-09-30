14a415b2c69122d74c11dde72605ce748019e4b8
Babita00 <babitaasth07@gmail.com>
2024-09-30 09:50:35 +0545

    path and element added

--- 07-ReactRouter/src/main.jsx
+++ 07-ReactRouter/src/main.jsx
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 // import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
+import Route from './route';
+import Home from "./Components/Home/Home"
+import About from './Components/About/About';
+import Contact from "./Components/Contact/Contact"
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router=createBrowserRouter([{
-  path:"/"
-},{},{}])
+  path:"/",
+  element:<Route/>,
+  children:[{
+    path:"home",
+    element:<Home/>
+  },{
+    path:"about",
+    element:<About/>
+  },{
+    path:"contact",
+    element:<Contact/>
+  }]
+}])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
