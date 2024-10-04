0d0007c8e49a1cccd13746f4c6b3f1297f9e16c2
Babita00 <babitaasth07@gmail.com>
2024-10-04 15:38:25 +0545

    update

--- 06-CurrencyConverter/src/components/InputBox.jsx
+++ 06-CurrencyConverter/src/components/InputBox.jsx
@@ -32,7 +32,7 @@ function InputBox({
                   placeholder="Amount"
                   disabled={amountDisable}
                   value={amount}
-                  onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}
+                  onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
               />
           </div>
           <div className="w-1/2 flex flex-wrap justify-end text-right">
@@ -40,13 +40,17 @@ function InputBox({
               <select
                   className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                   value={selectCurrency}
-                  onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)}
+                  onChange={(e) =>  {
+                    if (onCurrencyChange) {
+                      onCurrencyChange(e.target.value);
+                    }
+                  }} 
                   disabled={currencyDisable}
               >
                   {currencyOptions.map((currency)=>(
                     
                     <option key={currency} value={currency}>
-                    {currency}
+                    {currency.toUpperCase()}
                 </option>
                   ))}
               
