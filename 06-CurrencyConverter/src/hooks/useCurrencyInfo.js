208e3af728d2098ef9e53d3f62c447a37b3f278d
Babita00 <babitaasth07@gmail.com>
2024-10-06 09:33:05 +0545

    finally working all problem resolved

--- 06-CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ 06-CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -2,20 +2,22 @@ import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
-  //api call
+
   useEffect(() => {
     fetch(
       `https://v6.exchangerate-api.com/v6/227ff417209f96fb0be2aa43/latest/${currency}`
     )
+      .then((res) => res.json()) // Ensure the response is converted to JSON
       .then((res) => {
-        res.json();
-      })
-      .then((res) => {
-        console.log(res);
+        console.log(res); // Logging full response to debug
 
-        setData(res[currency]);
+        setData(res.conversion_rates); // Store conversion rates, not the entire response
+      })
+      .catch((error) => {
+        console.error("Error fetching currency data: ", error);
       });
   }, [currency]);
+
   return data;
 }
 
