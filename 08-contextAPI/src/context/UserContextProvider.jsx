6be24ae432ebf453896dc90811aad5aaa81d0964
Babita00 <babitaasth07@gmail.com>
2024-10-18 21:31:55 +0545

    userContext.Provider used

--- 08-contextAPI/src/context/UserContextProvider.jsx
+++ 08-contextAPI/src/context/UserContextProvider.jsx
@@ -1,7 +1,22 @@
 
+import { useState } from "react";
 import userContext from "./userContext";
+import PropTypes from 'prop-types'; 
+const UserContextProvider = ({children}) => {
 
-const UserContextProvider=({children})=>{
-return(<>{children}</>)
+const [user,setUser]=useState(null)
+return(
+    //wrapping the component
+
+<userContext.Provider value={{user,setUser}}>
+{children}
+</userContext.Provider>
+)
 }
+
+
+UserContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+  
 export default UserContextProvider
