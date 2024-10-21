f97e3d9697279551ba9008734645891f5c1022a3
Babita00 <babitaasth07@gmail.com>
2024-10-21 11:27:56 +0545

    login and profile wrapped into userContextProvider

--- 08-contextAPI/src/App.jsx
+++ 08-contextAPI/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Login from './components/login'
+import Profile from './components/Profile'
 // import ThemeChange from "./component/Theme/ThemeChange";
 import UserContextProvider from "./context/UserContextProvider"
 function App() {
 
   return (
     <UserContextProvider>
-    
+    <Login/>
+    <Profile/>
     </UserContextProvider>
   )
 }
