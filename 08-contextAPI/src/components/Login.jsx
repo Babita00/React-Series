28cb77162ccee68b8563da10d9b89151dd2d2896
Babita00 <babitaasth07@gmail.com>
2024-10-21 11:28:23 +0545

    Submit function

--- 08-contextAPI/src/components/Login.jsx
+++ 08-contextAPI/src/components/Login.jsx
@@ -6,13 +6,16 @@ function Login() {
     const [username,setUsername] = useState("")
     const [password,setPassword] = useState("")
     const {setUser}=useContext(userContext) //setUser from UserContext provider
-    const handleSubmit=()=>{
 
+    const handleSubmit=(e)=>{
+      e.preventDefault(); // Prevents page reload on form submission
+      setUser({username,password})
     }
   return (
     <div>
         <h2>Login</h2>
         <input type="text" value={username} onChange={(e)=> setUsername(e.target.value)} placeholder="username"/>
+        {"   "}
         <input type="text" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="password"/>
         <button onClick={handleSubmit}>Submit</button>
     </div>
