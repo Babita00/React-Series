963844dc8808d25d07e5142f72fec9b926446974
Babita00 <babitaasth07@gmail.com>
2024-10-23 20:43:29 +0545

    ui setup for app

--- 9-ThemeSwitcher/src/App.jsx
+++ 9-ThemeSwitcher/src/App.jsx
@@ -6,9 +6,23 @@ function App() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
+     {/* <h1 className="text-3xl font-bold underline p-4 text-pink-600 text-center">
       Hello world!
-    </h1>
+    </h1> */}
+
+    
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                       
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+
 
     </>
   )
