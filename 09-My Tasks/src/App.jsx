9f4899276ac2936264b9e170ed684adb064b969c
Babita00 <babitaasth07@gmail.com>
2024-10-23 09:23:18 +0545

    import component

--- 09-My Tasks/src/App.jsx
+++ 09-My Tasks/src/App.jsx
@@ -1,13 +1,16 @@
-
+import AuthContextProvider from "./context/AuthContextProvider"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
-    
-    </>
+    <AuthContextProvider>
+    <Login/>
+    <Profile/>
+    </AuthContextProvider>
   )
 }
 
