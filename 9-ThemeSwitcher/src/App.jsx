4e7a16a05654fe0cd7d437768373dbe9bfdd8074
Babita00 <babitaasth07@gmail.com>
2024-10-23 21:22:35 +0545

    wrapping inside ThemeProvider

--- 9-ThemeSwitcher/src/App.jsx
+++ 9-ThemeSwitcher/src/App.jsx
@@ -1,27 +1,28 @@
 
 import './App.css'
+import { ThemeProvider } from './context/Theme'
 
 function App() {
   
 
   return (
     <>
-     {/* <h1 className="text-3xl font-bold underline p-4 text-pink-600 text-center">
-      Hello world!
-    </h1> */}
-
     
+<ThemeProvider>
+  
 <div className="flex flex-wrap min-h-screen items-center">
   <div className="w-full">
     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
       {/* theme button */}
       </div>
-      
+
       <div className="w-full max-w-sm mx-auto">
         {/* card */}
       </div>
   </div>
 </div>
+
+</ThemeProvider>
 
 
     </>
