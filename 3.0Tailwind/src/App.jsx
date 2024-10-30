1c47feba476f2b9291ea557ebc48fd83afd0d0da
Babita00 <babitaasth07@gmail.com>
2024-10-30 19:27:14 +0545

    use of tailwind practice

---
+++ 3.0Tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import Mode from "./components/Mode";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-600"> Welcome to Tailwind class</h1>
+      <Mode />
+    </>
+  );
+}
+
+export default App;
