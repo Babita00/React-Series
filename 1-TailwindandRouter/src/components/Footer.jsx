5fc9d1aa301aa5689f9cd8ec56ac4658184a7c05
Babita00 <babitaasth07@gmail.com>
2024-11-01 23:33:35 +0545

    made footer

---
+++ 1-TailwindandRouter/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 flex justify-center">
+      <h1 className="text-4xl font-bold text-pink-600 mb-4">Babita</h1>
+    </div>
+  );
+}
+
+export default Footer;
