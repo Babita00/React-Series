11a2f4a85f0a362e7526c8b4bc6f462044bcbca1
Babita00 <babitaasth07@gmail.com>
2024-11-01 23:14:44 +0545

    remove unnecessary things

--- 1-TailwindandRouter/src/App.jsx
+++ 1-TailwindandRouter/src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import Colors from "./components/Colors";
-import Sizing from "./components/Sizing";
-import Topography from "./components/Topography";
-import Shadows from "./components/Shadows";
+
 function App() {
-  return (
-    <>
-      <Sizing />
-      <Colors />
-      <Topography />
-      <Shadows />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
