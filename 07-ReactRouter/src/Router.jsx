5a848241feb3187f908d0c9726210dc3dfc3269e
Babita00 <babitaasth07@gmail.com>
2024-11-01 22:50:48 +0545

    don't need home all time when we navigate to another link so comment out

--- 07-ReactRouter/src/Router.jsx
+++ 07-ReactRouter/src/Router.jsx
@@ -1,19 +1,17 @@
-import Header from "./Components/Header/header.jsx"
-import Home from "./Components/Home/Home.jsx"
-import Footer from './Components/Footer/Footer.jsx';
+import Header from "./Components/Header/header.jsx";
+// import Home from "./Components/Home/Home.jsx"
+import Footer from "./Components/Footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
-
 
 function Router() {
   return (
     <>
-    <Header/>
-    <Outlet/>
-   <Home/>
-   <Footer/>
+      <Header />
+      <Outlet />
+      {/* <Home/> */}
+      <Footer />
     </>
-  )
+  );
 }
 
-
-export default Router
+export default Router;
