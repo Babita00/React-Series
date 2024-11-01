e9bd8179d52bd8c537026eea458d6172dbbbb056
Babita00 <babitaasth07@gmail.com>
2024-11-01 23:33:27 +0545

    made router

---
+++ 1-TailwindandRouter/src/Router.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./Components/Footer";
+import { Outlet } from "react-router-dom";
+import Home from "./components/Home";
+
+function Router() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Outlet />
+
+      <Footer />
+    </>
+  );
+}
+
+export default Router;
