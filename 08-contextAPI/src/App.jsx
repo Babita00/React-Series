5d06216d05cd2a2147de4581423373d68bb98bd9
Babita00 <babitaasth07@gmail.com>
2024-11-02 10:29:43 +0545

    formatting

--- 08-contextAPI/src/App.jsx
+++ 08-contextAPI/src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css'
-import Login from './components/login'
-import Profile from './components/Profile'
+import "./App.css";
+import Login from "./components/login";
+import Profile from "./components/Profile";
 // import ThemeChange from "./component/Theme/ThemeChange";
-import UserContextProvider from "./context/UserContextProvider"
+import UserContextProvider from "./context/userContextProvider";
 function App() {
-
   return (
     <UserContextProvider>
-    <Login/>
-    <Profile/>
+      <Login />
+      <Profile />
     </UserContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- 08-contextAPI/src/context/UserContextProvider.jsx
+++ 08-contextAPI/src/context/UserContextProvider.jsx
@@ -1,25 +1,23 @@
 // Import necessary hooks and modules from React and other libraries
-import { useState} from "react";
-import userContext from "./UserContext"; // Importing the context
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import userContext from "./userContext.js";
+import PropTypes from "prop-types";
 
 // Defining the UserContextProvider component as a Context Provider
-const UserContextProvider = ({children}) => {
-
+const UserContextProvider = ({ children }) => {
   // State to manage the user data
-const [user,setUser]=useState(null)
-return(
+  const [user, setUser] = useState(null);
+  return (
     //wrapping the component
 
-<userContext.Provider value={{user,setUser}}>
-{children}
-</userContext.Provider>
-)
-}
-
+    <userContext.Provider value={{ user, setUser }}>
+      {children}
+    </userContext.Provider>
+  );
+};
 
 UserContextProvider.propTypes = {
-    children: PropTypes.node.isRequired,
-  };
-  
-export default UserContextProvider
+  children: PropTypes.node.isRequired,
+};
+
+export default UserContextProvider;
