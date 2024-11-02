b80b48596498558722e0d8f0a8bebbea514b9540
Babita00 <babitaasth07@gmail.com>
2024-11-02 11:59:58 +0545

    comment the topic

--- 08-contextAPI/src/context/UserContextProvider.jsx
+++ 08-contextAPI/src/context/UserContextProvider.jsx
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 // Defining the UserContextProvider component as a Context Provider
 const UserContextProvider = ({ children }) => {
   // State to manage the user data
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); //you can call api here
   return (
     //wrapping the component
 
