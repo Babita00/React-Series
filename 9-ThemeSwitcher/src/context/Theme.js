2ac059215ae3349721d40efcf603d967929e9e17
Babita00 <babitaasth07@gmail.com>
2024-11-02 12:02:09 +0545

    remove duplicate code

--- 9-ThemeSwitcher/src/context/Theme.js
+++ 9-ThemeSwitcher/src/context/Theme.js
@@ -1,42 +1,13 @@
-// Import the createContext function from React
 import { createContext, useContext } from "react";
 
-// Create a ThemeContext with default values
-
-// `themeMode`: Specifies the initial theme mode, set to "light" by default
-
-// `darkTheme`: Placeholder function to switch to dark mode (to be implemented later)
-// `lightTheme`: Placeholder function to switch to light mode (to be implemented later)
-
-// Export ThemeContext to be used across the app
-
 export const ThemeContext = createContext({
-  themeMode: "light", // Default theme mode
-  darkTheme: () => {}, // Function to apply dark theme (empty for now)
-  lightTheme: () => {}, // Function to apply light theme (empty for now)
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {},
 });
-
-// export const ThemeProvider = ({ children }) => {
-//   <ThemeContext.Provider>{children}</ThemeContext.Provider>;
-// };
 
 export const ThemeProvider = ThemeContext.Provider;
 
 export default function useTheme() {
   return useContext(ThemeContext);
 }
-
-//clear code
-// import { createContext, useContext } from "react";
-
-// export const ThemeContext = createContext({
-//     themeMode: "light",
-//     darkTheme: () => {},
-//     lightTheme: () => {},
-// })
-
-// export const ThemeProvider = ThemeContext.Provider
-
-// export default function useTheme(){
-//     return useContext(ThemeContext)
-// }
