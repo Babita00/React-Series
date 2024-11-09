bcb5b3f02f286910254f8144b8e7d984f6a35635
Babita00 <babitaasth07@gmail.com>
2024-11-09 14:58:09 +0545

    check if it is woring

--- 11-ReduxToolKit/src/App.jsx
+++ 11-ReduxToolKit/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="decoration-solid text-xl	font-serif text-amber-600">
+        Learn about Redux ToolKit
+      </h1>
+    </>
+  );
 }
 
 export default App;
