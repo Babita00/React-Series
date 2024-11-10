033bb36310dc7c590aafd86932d8a47bf8465a4c
Babita00 <babitaasth07@gmail.com>
2024-11-10 22:25:55 +0545

    apply selector and dispatch functionality

---
+++ 11-ReduxToolKit/src/components/Todos.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../Feature/Todo/TodoSlice";
+function Todos() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div>My Todos</div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
+        </li>
+      ))}
+    </>
+  );
+}
+
+export default Todos;
