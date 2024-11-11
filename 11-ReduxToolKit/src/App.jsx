ea4507400e05bfcabd056afaac652d057b067e39
Babita00 <babitaasth07@gmail.com>
2024-11-11 18:43:02 +0545

    render AddTodo and Todos components

--- 11-ReduxToolKit/src/App.jsx
+++ 11-ReduxToolKit/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
-
+import { AddTodo } from "./components/AddTodo";
+import Todos from "./components/Todos";
 function App() {
   return (
     <>
       <h1 className="decoration-solid text-xl	font-serif text-amber-600">
         Learn about Redux ToolKit
       </h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
