f639009dd1a91df6611bdae4ec359c83b56933a8
Babita00 <babitaasth07@gmail.com>
2024-12-12 14:30:28 +0545

    navigation

--- NetflicBasicClone/src/Components/Login.jsx
+++ NetflicBasicClone/src/Components/Login.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -10,20 +12,23 @@ function Login() {
       setError("Please fill in both email and password");
     } else {
       setError(null);
+      navigate("/home");
     }
   };
   return (
     <>
       <div className="flex justify-center item-center h-96 bg-dark ">
         <div className="bg-black  bg-opacity-85 w-96 rounded-lg">
-          <h2 className="text-xl text-white font-bold mt-4">Sign In</h2>
+          <h2 className="text-2xl text-white font-bold mt-4 flex justify-center item-center">
+            Sign In
+          </h2>
           <form onSubmit={handleSubmit}>
             <div className="mb-4 flex text-black ">
               <input
                 type="text"
                 value={email}
                 placeholder="Email or Mobile Number"
-                className="w-full p-2 m-2 border bg-black bg-opacity-85 text-white placeholder-white rounded-lg h-16"
+                className="w-full p-2 mx-2 border bg-black bg-opacity-85 text-white placeholder-white rounded-lg h-16"
                 onChange={(event) => setEmail(event.target.value)}
               />
             </div>
@@ -33,7 +38,7 @@ function Login() {
                 type="password"
                 value={password}
                 placeholder="Password"
-                className="w-full p-2 m-2 border bg-black bg-opacity-85 text-white placeholder-white rounded-lg h-16"
+                className="w-full p-2 mx-2 border bg-black bg-opacity-85 text-white placeholder-white rounded-lg h-16"
                 onChange={(event) => setPassword(event.target.value)}
               />
             </div>
@@ -41,7 +46,7 @@ function Login() {
 
             <button
               type="submit"
-              className="w-full m-2 h-16 bg-red-500 text-white rounded-lg hover:bg-red-300"
+              className="w-full mt-4 mx-2 h-16 bg-red-500 text-white rounded-lg hover:bg-red-300"
             >
               Sign In
             </button>
