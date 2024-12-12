c9c5c97018320f63dca3bea549bb9df1a8604507
Babita00 <babitaasth07@gmail.com>
2024-12-12 13:42:27 +0545

    create route

--- NetflicBasicClone/src/main.jsx
+++ NetflicBasicClone/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import "./index.css";
+import Home from "./Components/Home.jsx";
+import Router from "./Router.jsx";
+import Login from "./Components/Login.jsx";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Router />}>
+      <Route path="login" element={<Login />}></Route>
+      <Route path="home" element={<Home />}></Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
