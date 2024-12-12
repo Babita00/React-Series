1a09205d553ed57c309c05044977ad621d8a0839
Babita00 <babitaasth07@gmail.com>
2024-12-12 13:42:42 +0545

    define header and home

---
+++ NetflicBasicClone/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div className="">
+      <h1>Unlimited movies, TV shows, and more</h1>
+      <p>Starts at USD 2.99. Cancel anytime.</p>
+      <p>
+        Ready to watch? Enter your email to create or restart your membership.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
