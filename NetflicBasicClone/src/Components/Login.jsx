3cd0370ec4c61dc3a9c20fd35bae31e3b5589a9c
Babita00 <babitaasth07@gmail.com>
2024-12-12 13:20:42 +0545

    apply functionality

--- NetflicBasicClone/src/Components/Login.jsx
+++ NetflicBasicClone/src/Components/Login.jsx
@@ -1,35 +1,47 @@
 import { useState } from "react";
-function Lgoin() {
-  const [email, setEmail] = useState(" ");
+function Login() {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!email || !password) {
+      setError("Please fill in both email and password");
+    } else {
+      setError(null);
+    }
+  };
   return (
     <>
-      <div className="flex justify-center item-center h-screen">
-        <div className="bg-black  bg-opacity-85 w-96">
-          <h2 className="text-xl text-White font-bold mb-4">Sign In</h2>
-          <form>
-            <div className="mb-4 flex text-black h-30">
+      <div className="flex justify-center item-center h-96 bg-dark ">
+        <div className="bg-black  bg-opacity-85 w-96 rounded-lg">
+          <h2 className="text-xl text-white font-bold mt-4">Sign In</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4 flex text-black ">
               <input
                 type="text"
                 value={email}
                 placeholder="Email or Mobile Number"
-                className=" w-full p-2 mt-2 border bg-black bg-opacity-85 text-white placeholder-white rounded-lg"
+                className="w-full p-2 m-2 border bg-black bg-opacity-85 text-white placeholder-white rounded-lg h-16"
                 onChange={(event) => setEmail(event.target.value)}
               />
             </div>
-            <div className="mb-4 h-20">
-              <label htmlFor="password">Password</label>
+
+            <div className="mb-4 flex text-black ">
               <input
-                type="text"
+                type="password"
                 value={password}
+                placeholder="Password"
+                className="w-full p-2 m-2 border bg-black bg-opacity-85 text-white placeholder-white rounded-lg h-16"
                 onChange={(event) => setPassword(event.target.value)}
-                className="block w-full p-2 mt-2 border border-gray-300 rounded-lg"
               />
             </div>
+            {error && <div className="mb-4 text-red-500">{error}</div>}
 
             <button
               type="submit"
-              className="w-full py-2 px-4 bg-red-500 text-white rounded-lg hover:bg-red-300"
+              className="w-full m-2 h-16 bg-red-500 text-white rounded-lg hover:bg-red-300"
             >
               Sign In
             </button>
@@ -40,4 +52,4 @@ function Lgoin() {
   );
 }
 
-export default Lgoin;
+export default Login;
