8a826713d68ab08cc7a98ca3a394c1dfc46c171c
Babita00 <babitaasth07@gmail.com>
2024-12-12 14:30:55 +0545

    define css

--- NetflicBasicClone/src/Components/Home.jsx
+++ NetflicBasicClone/src/Components/Home.jsx
@@ -1,9 +1,11 @@
 function Home() {
   return (
-    <div className="">
-      <h1>Unlimited movies, TV shows, and more</h1>
-      <p>Starts at USD 2.99. Cancel anytime.</p>
-      <p>
+    <div className="bg-cover bg-center h-screen flex flex-col justify-center items-center text-center">
+      <h1 className="text-5xl font-bold">
+        Unlimited movies, TV shows, and more
+      </h1>
+      <p className="text-xl">Starts at USD 2.99. Cancel anytime.</p>
+      <p className="text-lg">
         Ready to watch? Enter your email to create or restart your membership.
       </p>
     </div>
