110e9a7cb68c8e412232eadb143e522a0bcb03e3
Babita00 <babitaasth07@gmail.com>
2024-12-13 13:50:01 +0545

    apply css

--- NetflicBasicClone/src/Components/Profile.jsx
+++ NetflicBasicClone/src/Components/Profile.jsx
@@ -5,32 +5,50 @@ function Profile() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState("");
 
-  // Dummy login information
-  const userId = "123"; // Example: Stored after login
+  const userId = "123";
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const response = await axios.get(
-          `https://jsonplaceholder.typicode.com/todos/10`
+          `https://jsonplaceholder.typicode.com/users/2`
         );
         setUser(response.data);
       } catch (error) {
-        setError("Failed to fetch user details", error);
+        setError("Failed to fetch user details");
       }
     };
 
     fetchUserData();
   }, [userId]);
 
-  if (error) return <p>{error}</p>;
-  if (!user) return <p>Loading...</p>;
+  if (error)
+    return (
+      <div className="flex items-center justify-center h-screen bg-red-50">
+        <p className="text-red-600 text-lg font-semibold">{error}</p>
+      </div>
+    );
+  if (!user)
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-50">
+        <p className="text-gray-700 text-lg font-medium">Loading...</p>
+      </div>
+    );
 
   return (
-    <div className="flex flex-col justify-center item-center">
-      <h1>Welcome, {user.name}</h1>
-      <p>Email: {user.email}</p>
-      <p>Username: {user.username}</p>
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">
+        Welcome, {user.name}
+      </h1>
+      <p className="text-gray-700 mb-2">
+        <strong>Email:</strong> {user.email}
+      </p>
+      <p className="text-gray-700 mb-2">
+        <strong>Username:</strong> {user.username}
+      </p>
+      <p className="text-gray-700">
+        <strong>Address:</strong> {user.address.city}
+      </p>
     </div>
   );
 }
